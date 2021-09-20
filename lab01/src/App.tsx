b6ed5c74f04867ed42f9ff31db38d48672376283
actpohabtNS <actpohabt.ns@gmail.com>
2021-09-20 13:37:30 +0300

    [A] desmos plot, method selection

--- lab01/src/App.tsx
+++ lab01/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex flex-column align-items-center">
+      <div id="calculator" style={{width: 900, height: 400}}></div>
+
+      <div className="mt-5">
+        <div className="form-check form-check-inline me-5">
+          <input checked className="form-check-input" type="radio" name="inlineRadioOptions" id="dichotomy" value="option1" />
+          <label className="form-check-label" htmlFor="dichotomy">Dichotomy</label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="iteration" value="option2" />
+          <label className="form-check-label" htmlFor="iteration">Iteration</label>
+        </div>    
+      </div>
+
+      <hr />
+
+      
     </div>
   );
 }
