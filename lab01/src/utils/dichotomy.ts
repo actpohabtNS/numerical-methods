7bf6b49454e0849ecca870c483aa585076ef579d
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-09-20 22:28:47 +0300

    [R] logs

--- lab01/src/utils/dichotomy.ts
+++ lab01/src/utils/dichotomy.ts
@@ -8,8 +8,6 @@ export function dichotomy(x0: number, a: number, b: number, epsilon: number, f: 
   }
 
   if (f(a) * f(b) > 0) {
-    console.log(`f(a): ${f(a)}, f(b): ${f(b)}`);
-    
     throw new Error("f(a) * f(b) must be less or equal than 0! ([a, x, b])");
   }
 
@@ -18,15 +16,10 @@ export function dichotomy(x0: number, a: number, b: number, epsilon: number, f: 
       return (f(a) === 0) ? a : b;
     }
 
-    console.log(`a: ${a}, b: ${b}, x0: ${x0}, f(x0): ${f(x0)}`);
-    
-
     a = (f(a) * f(x0) > 0) ? x0 : a;
     b = (f(b) * f(x0) > 0) ? x0 : b;
     x0 = (a + b) / 2;
   } while (Math.abs(f(x0)) > epsilon);
-
-  console.log(`a: ${a}, b: ${b}, x0: ${x0}, f(x0): ${f(x0)}`);
 
   return x0;
 }
