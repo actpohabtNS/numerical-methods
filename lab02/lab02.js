3c90af66be12f2969d3cb82f48df8504154dad80
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-11-17 13:43:30 +0200

    [Ref] added n - matrix size

--- lab02/lab02.js
+++ lab02/lab02.js
@@ -4,11 +4,13 @@ import { hasDiagonalAdvantage, solveJacobi } from './jacobi.js';
 import { genMatrix, genVector, genXFirst } from './generators.js';
 import { continuousVNorm } from './helpers.js';
 
+const n = 10;
+
 const gaussTask = () => {
   const m = genMatrix(10);
   const testA = [[10, 0, 3], [3, -1, 0], [-2, 4, 1]];
   const testB = [7, 2, 1];
-  const { solution, detA, invA, condA } = solveGauss(genMatrix(10), genVector(10));
+  const { solution, detA, invA, condA } = solveGauss(genMatrix(n), genVector(n));
   console.log("Solution:");
   printVector(solution, false);
   console.log("");
@@ -30,9 +32,9 @@ const jacobiTask = () => {
   const testA = [[3, -1, 1],[-1,2,0.5],[1,0.5,3]];
   const testB = [1, 1.75, 2.5];
   const testXFirst = [0,0,0];
-  printMatrix(genMatrix(10, 2));
+  printMatrix(genMatrix(4, 2));
   console.log("");
-  const { solution, iter_fact, iter_theor } = solveJacobi(genMatrix(10, 2), genVector(10), genXFirst(10), 0.001);
+  const { solution, iter_fact, iter_theor } = solveJacobi(genMatrix(n, 2), genVector(n), genXFirst(n), 0.001);
   printVector(solution, false);
   console.log("");
 
